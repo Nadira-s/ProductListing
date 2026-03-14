1de5f7a2728583f3437546fae292eaae204db378
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2026-03-14 17:18:22 +0500

    Initial Commit

---
+++ ProductListing/ProductListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListingApp.swift
+//  ProductListing
+//
+//  Created by Nadira Seitkazy  on 14.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ProductListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
